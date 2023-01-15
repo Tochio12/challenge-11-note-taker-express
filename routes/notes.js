186216304de927c13a9f88191b20ba5607b307d9
Melany Baldeon <tochio12910@gmail.com>
2023-01-15 00:28:48 -0500

    finally made everything work

--- routes/notes.js
+++ routes/notes.js
@@ -11,5 +11,23 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
+  notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text  } = req.body;
+  
+    if (req.body) {
+      const newNotes = {
+        title,
+        text,
+        notes_id: uuidv4(),
+      };
+  
+      readAndAppend(newNotes, './db/db.json');
+      res.json(`note added successfully 🚀`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
   module.exports = notes;
